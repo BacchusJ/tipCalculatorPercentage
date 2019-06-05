b01c8de79102ccf2a25a36ccca73a8497f10c90d
Vesper Jones <vesperjones@Vespers-MacBook-Pro.local>
2019-06-05 15:14:36 -0500

    added HTML with description of challange and also completed the requested challange

--- tipCalculartor.js
+++ tipCalculartor.js
@@ -12,9 +12,9 @@ var john = {
             if (bill < 50){
                 percentage = .2;
             }else if (bill >= 50 && bill < 200) {
-                percentage = .15
+                percentage = .15;
             } else {
-                percentage = .1
+                percentage = .1;
             }
             //add results to corresponding arrays
             this.tips[i] = bill * percentage;
@@ -22,5 +22,53 @@ var john = {
         }
     }
 }
+
+
+
+
+var mark = {
+    fullName: 'Mark Miller',
+    bills: [77, 475, 110, 45,],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+
+        for (var i = 0; i < this.bills.length; i++) {
+            //determine percentage
+            var percentage;
+            var bill = this.bills[i];
+            if (bill < 100){
+                percentage = .2;
+            }else if (bill >= 100 && bill < 300) {
+                percentage = .1;
+            } else {
+                percentage = .25;
+            }
+            //add results to corresponding arrays
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+        }
+    }
+}
+
+function calcAverage(tips) {
+    var sum = 0;
+    for (var i = 0; i < tips.length; i++) {
+        sum = sum + tips[i];
+    }
+    return sum / tips.length;
+}
+
+//do the average calculations 
 john.calcTips();
-console.log(john);
+mark.calcTips();
+
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+console.log(john, mark);
+
+if(john.average > mark.average) {
+    console.log(john.fullname + '\'s family pays higher tipes with an average of $' + john.average);   
+} else if (mark.average > john.average) {
+    console.log(mark.fullName + '\'s family pays higher tipes with an average of $' + mark.average);
+}
